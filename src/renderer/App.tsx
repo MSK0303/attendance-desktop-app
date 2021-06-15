90dead12b552f6d7ae2871c31e46671c82df06ba
MSK0303 <yoshihara@ltas03.com>
2021-06-15 22:07:30 +0900

    #1 基本的なベースレイアウトを行った

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,62 +1,29 @@
-import React from 'react'
-import logo from './assets/logo.svg'
+import React,{useState,useEffect} from 'react'
 import './App.css'
 import { hot } from 'react-hot-loader/root'
 import { remote } from 'electron'
 
-const App: React.FC = () => {
-  const electron = process.versions.electron
-  const node = process.versions.node
-  const platform = require('os').platform()
-  const version = require('../../package.json').version
+//react
+import {Grid} from '@material-ui/core';
 
-  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault()
-    remote.shell.openExternal(e.currentTarget.href)
-  }
+const App: React.FC = () => {
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul className="App-info">
-          <li>
-            electron: <span>{electron}</span>
-          </li>
-          <li>
-            node: <span>{node}</span>
-          </li>
-          <li>
-            platform: <span>{platform}</span>
-          </li>
-          <li>
-            version: <span>{version}</span>
-          </li>
-        </ul>
-        <p>
-          Edit <code>src/renderer/App.tsx</code> and save to reload.
-        </p>
-        <div>
-          <a
-            onClick={handleLinkClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <a
-            onClick={handleLinkClick}
-            className="App-link"
-            href="https://electronjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Electron
-          </a>
-        </div>
-      </header>
+    <div className="main-view">
+      <Grid container className="grid-top">
+        <Grid item xs={6} className="grid-calender">
+          Calender
+        </Grid>
+        <Grid item xs={6} className="grid-contents">
+          <Grid container className="grid-buttons">
+            Buttons
+          </Grid>
+          <Grid container className="grid-note">
+            Pre
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   )
 }
