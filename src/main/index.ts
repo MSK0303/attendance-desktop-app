f365b18555065c9f4bc8478262f53fef29580347
MSK0303 <yoshihara@ltas03.com>
2021-06-15 23:00:39 +0900

    #1 右側のコンテンツのヘッダーを作成

--- src/main/index.ts
+++ src/main/index.ts
@@ -25,6 +25,8 @@ function createWindow() {
     height: 563,
     useContentSize: true,
     width: 1000,
+    minWidth:800,
+    minHeight: 563,
     webPreferences: {
       nodeIntegration: true
     }
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,7 +7,7 @@ import { remote } from 'electron'
 import {Grid} from '@material-ui/core';
 
 const App: React.FC = () => {
-
+  const [currentDate,setCurrentDate] = useState("2021/6/11(金)");
 
   return (
     <div className="main-view">
@@ -16,8 +16,14 @@ const App: React.FC = () => {
           Calender
         </Grid>
         <Grid item xs={6} className="grid-contents">
+          <Grid container className="grid-date-line" justify="center" alignItems="center">
+            {currentDate}
+          </Grid>
           <Grid container className="grid-buttons">
             Buttons
+          </Grid>
+          <Grid container className="grid-note-header" justify="center" alignItems="center">
+            履歴
           </Grid>
           <Grid container className="grid-note">
             Pre
