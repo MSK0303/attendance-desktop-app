432e99d6b1ed92055ecfb1f4da74b186c34c5be1
MSK0303 <yoshihara@ltas03.com>
2021-06-21 17:29:26 +0900

    #2 カレンダー操作の確認

--- src/main/index.ts
+++ src/main/index.ts
@@ -22,11 +22,11 @@ function createWindow() {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 650,
     useContentSize: true,
     width: 1000,
     minWidth:950,
-    minHeight: 563,
+    minHeight: 650,
     webPreferences: {
       nodeIntegration: true
     }
--- src/renderer/components/AttCalendar/AttCalendar.tsx
+++ src/renderer/components/AttCalendar/AttCalendar.tsx
@@ -1,11 +1,59 @@
 import React,{useState,useEffect} from 'react'
-import Calendar from 'react-calendar';
+import Calendar,{ViewCallbackProperties,DrillCallbackProperties} from 'react-calendar';
 import './AttCalendar.css';
 
 const AttCalendar = () => {
+
+    const onChange = (value: Date, event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("onChange");
+    }
+    const onViewChange = (props:ViewCallbackProperties) => {
+        console.log("onViewChange");
+    }
+
+    const onClickDay = (value: Date, event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("onClickDay");
+    }
+
+    const onClickDecade = (value: Date, event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("onClickDecade");
+    }
+
+    const onClickMonth = (value: Date, event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("onClickMonth");
+    }
+
+    const onClickWeekNumber = (weekNumber: number, date: Date, event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("onClickWeekNumber");
+    }
+
+    const onClickYear = (value: Date, event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("onClickYear");
+    }
+
+    const onDrillUp = (props: DrillCallbackProperties) => {
+        console.log("onDrillUp");
+    }
+    const onDrillDown = (props: DrillCallbackProperties) => {
+        console.log("onDrillDown");
+    }
+
+
+
     return (
         <div className="calendar-top">
-            <Calendar className="calendar-comp"/>
+            <Calendar 
+            locale="ja-JP"
+            onChange={onChange}
+            onViewChange={onViewChange}
+            onClickDay={onClickDay}
+            onClickDecade={onClickDecade}
+            onClickMonth={onClickMonth}
+            onClickWeekNumber={onClickWeekNumber}
+            onClickYear={onClickYear}
+            onDrillUp={onDrillUp}
+            onDrillDown={onDrillDown}
+            />
         </div>
     )
 }
